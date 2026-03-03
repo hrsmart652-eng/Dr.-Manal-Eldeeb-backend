c4e96a20257c883f62474b717aae25818d115e51
nermin-create <57436386+nermin-create@users.noreply.github.com>
2026-03-03 14:48:52 +0200

    cros file

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,32 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+  
+      'paths' => ['api/*'], // API routes only
+ 'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        'Content-Type',
+        'X-Requested-With',
+        'Authorization',
+        'Accept',
+        'Origin',
+        'X-CSRF-TOKEN',
+    ],
 
-    'exposed_headers' => [],
+    'exposed_headers' => [
+        'Authorization',
+        'Content-Type',
+        'X-Requested-With',
+    ],
 
-    'max_age' => 0,
+    'max_age' => 3600,
 
-    'supports_credentials' => false,
+ 
+    'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', false), // false by default, set to true if you need to support cookies or HTTP authentication
 
 ];
+
